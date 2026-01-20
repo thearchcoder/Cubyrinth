93c134ca9b81c85907c6013aec92980140a1e0ac
TheArchCoder <ahmadchawla1432@gmail.com>
2026-01-20 13:01:35 +0500

    Switch to device rotation for ball movement

--- Assets/Scripts/SwipeBall.cs
+++ Assets/Scripts/SwipeBall.cs
@@ -12,10 +12,8 @@ public enum BallAxis
 public class SwipeBall : MonoBehaviour
 {
 	[SerializeField] private BallAxis m_Face = BallAxis.ZPositive;
-	[SerializeField] private float m_DragForceMultiplier = 0.006f;
+	[SerializeField] private float m_GravityMultiplier = 9.81f;
 	private Rigidbody m_Rigidbody;
-	private bool m_IsDragging;
-	private Vector2 m_LastPosition;
 	void Start()
 	{
 		m_Rigidbody = GetComponent<Rigidbody>();
@@ -23,6 +21,7 @@ public class SwipeBall : MonoBehaviour
 		{
 			m_Rigidbody = gameObject.AddComponent<Rigidbody>();
 		}
+		m_Rigidbody.useGravity = false;
 
 		GameObject maze_walls = GameObject.FindGameObjectWithTag("MazeOutsideWalls");
 		if (maze_walls != null)
@@ -33,85 +32,41 @@ public class SwipeBall : MonoBehaviour
 				renderer.enabled = false;
 			}
 		}
-	}
 
-	void Update()
-	{
-		if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
+		if (Accelerometer.current != null)
 		{
-			HandleTouch();
-		}
-		else
-		{
-			HandleMouse();
+			InputSystem.EnableDevice(Accelerometer.current);
 		}
 	}
-	void HandleTouch()
+	void FixedUpdate()
 	{
-		var touch = Touchscreen.current.primaryTouch;
-		if (touch.press.wasPressedThisFrame)
+		if (Accelerometer.current != null)
 		{
-			m_IsDragging = true;
-			m_LastPosition = touch.position.ReadValue();
-		}
-		else if (m_IsDragging)
-		{
-			Vector2 current_position = touch.position.ReadValue();
-			ApplyDragForce(current_position);
-		}
-		if (touch.press.wasReleasedThisFrame)
-		{
-			m_IsDragging = false;
-			m_Rigidbody.linearVelocity = Vector3.zero;
+			Vector3 accel = Accelerometer.current.acceleration.ReadValue();
+			Vector3 gravity = GetGravityDirection(accel) * m_GravityMultiplier;
+			m_Rigidbody.AddForce(gravity, ForceMode.Acceleration);
+			ConstrainVelocity();
 		}
 	}
-	void HandleMouse()
-	{
-		if (Mouse.current == null) return;
-		if (Mouse.current.leftButton.wasPressedThisFrame)
-		{
-			m_IsDragging = true;
-			m_LastPosition = Mouse.current.position.ReadValue();
-		}
-		else if (Mouse.current.leftButton.isPressed && m_IsDragging)
-		{
-			Vector2 current_position = Mouse.current.position.ReadValue();
-			ApplyDragForce(current_position);
-		}
-		else if (Mouse.current.leftButton.wasReleasedThisFrame)
-		{
-			m_IsDragging = false;
-			m_Rigidbody.linearVelocity = Vector3.zero;
-		}
-	}
-	Vector3 GetForceDirection(Vector2 delta)
+	Vector3 GetGravityDirection(Vector3 accel)
 	{
 		switch (m_Face)
 		{
 			case BallAxis.XPositive:
-				return new Vector3(0f, delta.y, -delta.x);
+				return new Vector3(0f, -accel.y, accel.x);
 			case BallAxis.XNegative:
-				return new Vector3(0f, delta.y, delta.x);
+				return new Vector3(0f, -accel.y, -accel.x);
 			case BallAxis.YPositive:
-				return new Vector3(delta.x, 0f, delta.y);
+				return new Vector3(-accel.x, 0f, -accel.y);
 			case BallAxis.YNegative:
-				return new Vector3(delta.x, 0f, -delta.y);
+				return new Vector3(-accel.x, 0f, accel.y);
 			case BallAxis.ZPositive:
-				return new Vector3(delta.x, delta.y, 0f);
+				return new Vector3(-accel.x, -accel.y, 0f);
 			case BallAxis.ZNegative:
-				return new Vector3(-delta.x, delta.y, 0f);
+				return new Vector3(accel.x, -accel.y, 0f);
 			default:
-				return new Vector3(delta.x, delta.y, 0f);
+				return new Vector3(-accel.x, -accel.y, 0f);
 		}
-	}
-	void ApplyDragForce(Vector2 current_position)
-	{
-		Vector2 delta = current_position - m_LastPosition;
-		Vector3 force = GetForceDirection(delta) * m_DragForceMultiplier;
-
-		m_Rigidbody.linearVelocity = force;
-		m_LastPosition = current_position;
-		ConstrainVelocity();
 	}
 	void ConstrainVelocity()
 	{
